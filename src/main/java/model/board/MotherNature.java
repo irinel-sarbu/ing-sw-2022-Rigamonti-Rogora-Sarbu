d38103b5d36e7b8b03c91470829faf3132d41af2
Alberto Rigamonti <alberto1.rigamonti@mail.polimi.it>
2022-03-19 12:45:26 +0100

    modified MotherNature

--- src/main/java/model/board/MotherNature.java
+++ src/main/java/model/board/MotherNature.java
@@ -1,4 +1,17 @@
 package model.board;
 
 public class MotherNature {
+    private int position;
+
+    public MotherNature(){
+        this.position=0;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void progress(int steps, int islands) {
+        this.position = (this.position + steps) % islands;
+    }
 }
