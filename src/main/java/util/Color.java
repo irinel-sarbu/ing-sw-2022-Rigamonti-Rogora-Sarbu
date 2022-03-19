e8b997c06952cda60f1a5b6406a57050e8dca239
Razvan Irinel Sarbu <razvanirinel.sarbu@gmail.com>
2022-03-19 15:16:28 +0100

    Numerated colors

--- src/main/java/util/Color.java
+++ src/main/java/util/Color.java
@@ -1,9 +1,19 @@
 package util;
 
 public enum Color {
-    YELLOW,
-    BLUE,
-    GREEN,
-    RED,
-    PINK
+    YELLOW(0),
+    BLUE(1),
+    GREEN(2),
+    RED(3),
+    PINK(4);
+
+    private int value;
+
+    private Color(int num) {
+        this.value = num;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
