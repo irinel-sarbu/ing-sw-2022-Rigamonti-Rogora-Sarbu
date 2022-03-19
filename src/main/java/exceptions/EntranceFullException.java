c28a7d6d5ac90eb42ac3ac16b84a5b2600363a2f
Razvan Irinel Sarbu <razvanirinel.sarbu@gmail.com>
2022-03-19 15:35:04 +0100

    Edited SchoolBoard

---
+++ src/main/java/exceptions/EntranceFullException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class EntranceFullException extends Exception {
+    public EntranceFullException() {
+        super();
+    }
+
+    public EntranceFullException(String message) {
+        super(message);
+    }
+
+}
--- src/main/java/model/board/SchoolBoard.java
+++ src/main/java/model/board/SchoolBoard.java
@@ -1,18 +1,29 @@
 package model.board;
 
-import util.Color;
-import util.TowerColor;
-
 import java.util.*;
+
+import exceptions.EntranceFullException;
 
 public class SchoolBoard {
     private final List<Student> entrance;
-    private int[] table; //yellow = 0, blue = 1, green = 2, red = 3, pink = 4;
+    private int[] diningRoom; // yellow = 0, blue = 1, green = 2, red = 3, pink = 4;
     private final List<Professor> professors;
 
     public SchoolBoard() {
         this.entrance = new ArrayList<>();
-        table = new int[5];
+        diningRoom = new int[5];
         professors = new ArrayList<>();
+    }
+
+    public List<Student> getEntranceStudents() {
+        return entrance;
+    }
+
+    public void addToEntrance(Student... students) throws EntranceFullException {
+        for (Student student : students) {
+            boolean success = entrance.add(student);
+            if (!success)
+                throw new EntranceFullException();
+        }
     }
 }
