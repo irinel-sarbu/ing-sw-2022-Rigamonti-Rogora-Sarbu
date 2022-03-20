1fd336ad1ec773bd42e7b60c8bc9bbe26f36a453
Matteo Rogora <matteo.rogora@live.it>
2022-03-20 12:06:21 +0100

    Added interface comparable to IslandTile and methods of IslandGroup

--- src/main/java/model/board/IslandGroup.java
+++ src/main/java/model/board/IslandGroup.java
@@ -1,4 +1,65 @@
 package model.board;
 
+import exceptions.IllegalIslandGroupJoinException;
+import exceptions.NullIslandGroupException;
+import exceptions.NullTowerColorException;
+import util.Color;
+import util.TowerColor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class IslandGroup {
+    private final List<IslandTile> islands;
+
+    public IslandGroup(){
+        islands = new ArrayList<>();
+        islands.add(new IslandTile());
+    }
+
+    public List<Integer> getIslandTilesID(){
+        return islands.stream().map(IslandTile::getIslandID).collect(Collectors.toList());
+    }
+
+    public IslandGroup(IslandGroup islandGroup){
+        islands = new ArrayList<>();
+        islands.addAll(islandGroup.islands);
+    }
+
+    public int getSize(){
+        return islands.size();
+    }
+
+    public void setTowersColor(TowerColor towerColor) throws NullTowerColorException {
+        if (towerColor == null) throw new NullTowerColorException();
+        for(int i = 0; i<this.getSize(); i++){
+            islands.get(i).setTowerColor(towerColor);
+        }
+    }
+
+    public TowerColor getTowersColor() {
+        return islands.get(0).getTowerColor();
+    }
+
+    public int getStudentsNumber(Color color){
+        int total = 0;
+        for(IslandTile island : islands){
+            total += island.getStudentsNumber(color);
+        }
+        return total;
+    }
+
+    public IslandGroup join(IslandGroup other) throws IllegalIslandGroupJoinException, NullIslandGroupException {
+        if (other == null) throw new NullIslandGroupException();
+        if (! this.getTowersColor().equals(other.getTowersColor())) throw new IllegalIslandGroupJoinException();
+
+        IslandGroup newIslandGroup = new IslandGroup(this);
+        newIslandGroup.islands.addAll(other.islands);
+        Collections.sort(newIslandGroup.islands);
+
+        return newIslandGroup;
+    }
+
 }
--- src/main/java/model/board/IslandTile.java
+++ src/main/java/model/board/IslandTile.java
@@ -1,22 +1,22 @@
 package model.board;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import util.TowerColor;
 import util.Color;
 
-public class IslandTile {
+public class IslandTile implements Comparable<IslandTile> {
     private List<Student> students;
     private TowerColor towerColor;
     private Integer islandID;
+    private static int count = 0;
 
-    public IslandTile(int islandID) {
+    public IslandTile() {
         students = new ArrayList<>();
         towerColor = null;
-        this.islandID = islandID;
+        this.islandID = count;
+        count ++;
     }
 
     public int getIslandID() {
@@ -36,16 +36,20 @@ public class IslandTile {
     }
 
     public int getStudentsNumber(Color color) {
-        return (int) students.stream() // TODO: controlla se serve un Optional
+        return (int) students.stream()
                 .filter(s -> s.getColor().equals(color))
                 .count();
     }
 
     public Color getMostNumerous() {
-        return students.stream() // TODO: devo imparare la prog. func.
-                .filter(x -> x != null)
+        return students.stream()
+                .filter(Objects::nonNull)
                 .collect(Collectors.groupingBy(Student::getColor, Collectors.counting()))
                 .entrySet().stream().max(Map.Entry.comparingByValue())
                 .map(Map.Entry::getKey).orElse(null);
+    }
+
+    public int compareTo ( IslandTile other){
+        return Integer.compare(this.getIslandID(), other.getIslandID());
     }
 }
