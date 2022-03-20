73cb86e602ab89082dcbbf7470b1421e85ad2eca
Alberto Rigamonti <alberto1.rigamonti@mail.polimi.it>
2022-03-20 14:58:30 +0100

    modified NoEntryTile

--- src/main/java/model/expert/NoEntryTile.java
+++ src/main/java/model/expert/NoEntryTile.java
@@ -1,4 +1,17 @@
 package model.expert;
 
 public class NoEntryTile {
+    private int position; //-1 when not on a tile
+
+    public NoEntryTile(){
+        this.position = -1;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
 }
