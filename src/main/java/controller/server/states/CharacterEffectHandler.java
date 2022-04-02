386bd9838ea7e0670862e4d7dff4ef1c1b16826f
Matteo Rogora <matteo.rogora@live.it>
2022-04-02 13:25:46 +0200

    optimized herald effect

--- src/main/java/controller/server/states/CharacterEffectHandler.java
+++ src/main/java/controller/server/states/CharacterEffectHandler.java
@@ -60,14 +60,12 @@ public class CharacterEffectHandler {
             int playerPosition;
 
             // calculates the influence of each player and stores it in islandSum in the relative position
-            if (tempIslandGroup.getIslandTileByID(0).getHasTower()) {
-                islandSum = checkMostInfluenceWithTower(tempGame, tempIslandGroup);
-            } else {
-                islandSum= checkMostInfluenceWithoutTower(tempGame, tempIslandGroup);
-            }
+
+            islandSum = checkMostInfluence(tempGame, tempIslandGroup, tempIslandGroup.getIslandTileByID(0).getHasTower());
+
             // check which player has the most influence and, if there is one, changes island's tower to his color
-            playerPosition=playerId(islandSum);
-            if(playerPosition!=-1){
+            playerPosition = playerId(islandSum);
+            if (playerPosition != -1) {
                 tempIslandGroup.setTowersColor(tempGame.getPlayerByID(playerPosition).getColor());
             }
             // joins adjacent islandGroups
@@ -77,18 +75,27 @@ public class CharacterEffectHandler {
         }
     }
 
-    private int[] checkMostInfluenceWithTower(GameModel tempGame, IslandGroup tempIslandGroup) throws PlayerNotFoundException {
+    private int[] checkMostInfluence(GameModel tempGame, IslandGroup tempIslandGroup, boolean computeTowers) throws PlayerNotFoundException {
+        int[] islandSum = new int[tempGame.getPlayers().size()];
+        for (int i = 0; i < tempGame.getPlayers().size(); i++) {
+            if (computeTowers && tempIslandGroup.getIslandTileByID(0).getTowerColor() == tempGame.getPlayerByID(i).getColor()) {
+                islandSum[i]++;
+            }
+            for (Professor professor : tempGame.getPlayerByID(i).getSchoolBoard().getProfessors()) {
+                islandSum[i] += tempIslandGroup.getStudentsNumber(professor.getColor());
+            }
+        }
+        return islandSum;
+    }
+
+    /*private int[] checkMostInfluenceWithTower(GameModel tempGame, IslandGroup tempIslandGroup) throws PlayerNotFoundException {
         int[] islandSum = new int[tempGame.getPlayers().size()];
         for (int i = 0; i < tempGame.getPlayers().size(); i++) {
             if (tempIslandGroup.getIslandTileByID(0).getTowerColor() == tempGame.getPlayerByID(i).getColor()) {
                 islandSum[i]++;
-                for (Professor professor : tempGame.getPlayerByID(i).getSchoolBoard().getProfessors()) {
-                    islandSum[i] += tempIslandGroup.getStudentsNumber(professor.getColor());
-                }
-            } else {
-                for (Professor professor : tempGame.getPlayerByID(i).getSchoolBoard().getProfessors()) {
-                    islandSum[i] += tempIslandGroup.getStudentsNumber(professor.getColor());
-                }
+            }
+            for (Professor professor : tempGame.getPlayerByID(i).getSchoolBoard().getProfessors()) {
+                islandSum[i] += tempIslandGroup.getStudentsNumber(professor.getColor());
             }
         }
         return islandSum;
@@ -102,7 +109,7 @@ public class CharacterEffectHandler {
             }
         }
         return islandSum;
-    }
+    }*/
 
     private int playerId(int[] islandSum){
         int max1 = 0, max2 = 0;
