0d938e1231ae472e924cc9566ad003ce3774fc68
Alberto Rigamonti <alberto1.rigamonti@mail.polimi.it>
2022-04-09 11:44:24 +0200

    optimized duplicated code

--- src/main/java/controller/server/states/CentaurResolveIsland.java
+++ src/main/java/controller/server/states/CentaurResolveIsland.java
@@ -8,28 +8,6 @@ import model.board.Professor;
 import util.CharacterType;
 
 public class CentaurResolveIsland extends ResolveIsland{
-    public void solveIsland(GameLobby tempLobby, int islandGroupID) {
-        try {
-            GameModel tempGame = tempLobby.getModel();
-            int[] islandSum;
-            IslandGroup tempIslandGroup = tempGame.getIslandGroupByID(islandGroupID);
-            int playerPosition;
-
-            // calculates the influence of each player and stores it in islandSum in the relative position
-
-            islandSum = checkMostInfluence(tempLobby, tempGame, tempIslandGroup, tempIslandGroup.getIslandTileByID(0).getHasTower());
-
-            // check which player has the most influence and, if there is one, changes island's tower to his color
-            playerPosition = playerID(islandSum);
-            if (playerPosition != -1) {
-                tempIslandGroup.setTowersColor(tempGame.getPlayerByID(playerPosition).getColor());
-            }
-            // joins adjacent islandGroups
-            tempGame.joinAdjacent(islandGroupID);
-        } catch (PlayerNotFoundException e) {
-            // TODO : write a line of text that notify the issue
-        }
-    }
 
     protected int[] checkMostInfluence(GameLobby tempLobby, GameModel tempGame, IslandGroup tempIslandGroup, boolean computeTowers) throws PlayerNotFoundException {
         int[] islandSum = new int[tempGame.getPlayers().size()];
