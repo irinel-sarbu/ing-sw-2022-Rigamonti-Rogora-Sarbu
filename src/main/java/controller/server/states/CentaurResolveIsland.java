7d99a60ea1514161b7bc7e2cffb692dd07504178
Alberto Rigamonti <alberto1.rigamonti@mail.polimi.it>
2022-04-09 11:33:58 +0200

    Implemented CentaurResolveIsland, adjusted ResolveIsland methods

--- src/main/java/controller/server/states/CentaurResolveIsland.java
+++ src/main/java/controller/server/states/CentaurResolveIsland.java
@@ -1,5 +1,50 @@
 package controller.server.states;
 
-public class CentaurResolveIsland extends DefaultResolveIsland {
+import controller.server.GameLobby;
+import exceptions.PlayerNotFoundException;
+import model.GameModel;
+import model.board.IslandGroup;
+import model.board.Professor;
+import util.CharacterType;
+
+public class CentaurResolveIsland extends ResolveIsland{
+    public void solveIsland(GameLobby tempLobby, int islandGroupID) {
+        try {
+            GameModel tempGame = tempLobby.getModel();
+            int[] islandSum;
+            IslandGroup tempIslandGroup = tempGame.getIslandGroupByID(islandGroupID);
+            int playerPosition;
+
+            // calculates the influence of each player and stores it in islandSum in the relative position
+
+            islandSum = checkMostInfluence(tempLobby, tempGame, tempIslandGroup, tempIslandGroup.getIslandTileByID(0).getHasTower());
+
+            // check which player has the most influence and, if there is one, changes island's tower to his color
+            playerPosition = playerID(islandSum);
+            if (playerPosition != -1) {
+                tempIslandGroup.setTowersColor(tempGame.getPlayerByID(playerPosition).getColor());
+            }
+            // joins adjacent islandGroups
+            tempGame.joinAdjacent(islandGroupID);
+        } catch (PlayerNotFoundException e) {
+            // TODO : write a line of text that notify the issue
+        }
+    }
+
+    protected int[] checkMostInfluence(GameLobby tempLobby, GameModel tempGame, IslandGroup tempIslandGroup, boolean computeTowers) throws PlayerNotFoundException {
+        int[] islandSum = new int[tempGame.getPlayers().size()];
+        for (int i = 0; i < tempGame.getPlayers().size(); i++) {
+            if (computeTowers && tempIslandGroup.getIslandTileByID(0).getTowerColor() == tempGame.getPlayerByID(i).getColor()) {
+                //checks for passive effect of CENTAUR
+                if (tempLobby.getModel().getCharacterByType(CharacterType.CENTAUR) == null || !tempLobby.getModel().getCharacterByType(CharacterType.CENTAUR).getEffect()) {
+                    islandSum[i]++;
+                }
+            }
+            for (Professor professor : tempGame.getPlayerByID(i).getSchoolBoard().getProfessors()) {
+                islandSum[i] += tempIslandGroup.getStudentsNumber(professor.getColor());
+            }
+        }
+        return islandSum;
+    }
 
 }
--- src/main/java/controller/server/states/ResolveIsland.java
+++ src/main/java/controller/server/states/ResolveIsland.java
@@ -1,13 +1,36 @@
 package controller.server.states;
 
+import controller.server.GameLobby;
+import exceptions.PlayerNotFoundException;
+import model.GameModel;
+import model.board.IslandGroup;
+
 public abstract class ResolveIsland {
 
-    public abstract void resolveIsland();
+    public abstract void solveIsland(GameLobby tempLobby, int islandGroupID);
 
-    protected abstract void checkMostInfluence();
+    protected abstract int[] checkMostInfluence(GameLobby tempLobby, GameModel tempGame, IslandGroup tempIslandGroup, boolean computeTowers) throws PlayerNotFoundException;
 
-    protected void playerID() {
-
+    protected int playerID(int[] islandSum) {
+        int max1 = 0, max2 = 0;
+        int pos = 0;
+        for (int i = 0; i < islandSum.length; i++) {
+            if (islandSum[i] > max1) {
+                max1 = islandSum[i];
+                pos = i;
+            }
+        }
+        islandSum[pos] = 0;
+        for (int j : islandSum) {
+            if (j > max2) {
+                max2 = j;
+            }
+        }
+        if (max1 == max2) {
+            return -1;
+        } else {
+            return pos;
+        }
     }
 
 }
