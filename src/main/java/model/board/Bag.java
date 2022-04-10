681f0d4b083b9c3e995fb0c4b9a5dba2d67f8edf
Matteo Rogora <matteo.rogora@live.it>
2022-04-10 13:00:36 +0200

    written javadoc for half the board model

--- src/main/java/model/board/Bag.java
+++ src/main/java/model/board/Bag.java
@@ -10,6 +10,11 @@ public class Bag {
     private final List<Student> studentList;
     private int studentIdCount = 0;
 
+    /**
+     * Bag generic constructor for a game
+     *
+     * @param num specify number of new students of each color to put in the bag
+     */
     public Bag(int num) {
         this.studentList = new ArrayList<>();
         for (Color color : Color.values()) {
@@ -20,6 +25,15 @@ public class Bag {
         }
     }
 
+    /**
+     * Bag specific constructor for a game
+     *
+     * @param yellow specify number of yellow students to put in the bag
+     * @param blue   specify number of blue students to put in the bag
+     * @param green  specify number of green students to put in the bag
+     * @param red    specify number of red students to put in the bag
+     * @param pink   specify number of pink students to put in the bag
+     */
     public Bag(int yellow, int blue, int green, int red, int pink) {
         this.studentList = new ArrayList<>();
 
@@ -28,7 +42,7 @@ public class Bag {
             studentIdCount += 1;
         }
 
-        for (int b = 0; b < blue; b++){
+        for (int b = 0; b < blue; b++) {
             this.studentList.add(new Student(studentIdCount, Color.BLUE));
             studentIdCount += 1;
         }
@@ -38,7 +52,7 @@ public class Bag {
             studentIdCount += 1;
         }
 
-        for (int r = 0; r < red; r++){
+        for (int r = 0; r < red; r++) {
             this.studentList.add(new Student(studentIdCount, Color.RED));
             studentIdCount += 1;
         }
@@ -49,18 +63,36 @@ public class Bag {
         }
     }
 
+    /**
+     * Getter for remaining students
+     *
+     * @return number of students still inside the bag
+     */
     public int getRemainingStudents() {
         return studentList.size();
     }
 
+    /**
+     * Check if bag is empty
+     *
+     * @return true if the bag is empty, false otherwise
+     */
     public boolean isEmpty() {
         return getRemainingStudents() == 0;
     }
 
+    /**
+     * Shuffle bag content to randomize student extraction
+     */
     private void shuffle() {
         Collections.shuffle(studentList);
     }
 
+    /**
+     * Put given students into bag
+     *
+     * @param students list of students
+     */
     public void put(Student... students) {
         studentList.addAll(Arrays.asList(students));
         shuffle();
@@ -72,6 +104,12 @@ public class Bag {
         return studentList.remove(0);
     }
 
+    /**
+     * Allow toString to print additional content using a parameter
+     *
+     * @param printContent specify to print exact number of students for each color
+     * @return A string indicating remaining students and bag content if specified
+     */
     public String toString(boolean printContent) {
         String stringContent = printContent ? studentList.stream()
                 .filter(Objects::nonNull)
@@ -82,6 +120,11 @@ public class Bag {
         return "Bag: " + studentList.size() + " students remaining" + stringContent;
     }
 
+    /**
+     * Overrides toString
+     *
+     * @return A string indicating number of remaining students by calling {@link Bag#toString(false)}
+     */
     @Override
     public String toString() {
         return toString(false);
--- src/main/java/model/board/CloudTile.java
+++ src/main/java/model/board/CloudTile.java
@@ -12,32 +12,64 @@ public class CloudTile {
     private final List<Student> studentList;
     private final int maxSize;
 
+    /**
+     * CloudTile constructor
+     *
+     * @param cloudTileID specify {@link CloudTile#cloudTileID}
+     * @param size        specify maximum students movable on the tile
+     */
     public CloudTile(int cloudTileID, int size) {
         this.cloudTileID = cloudTileID;
         this.studentList = new ArrayList<>();
         this.maxSize = size;
     }
 
+    /**
+     * Check if cloudTile is empty
+     *
+     * @return true if cloud tile is empty, false otherwise
+     */
     public boolean isEmpty() {
         return this.studentList.isEmpty();
     }
 
+    /**
+     * Put students inside on the cloud tile
+     *
+     * @param students list of students as parameters
+     * @throws TooManyStudentsException if the cloud tile exceeds it's {@link CloudTile#maxSize}
+     */
     public void put(Student... students) throws TooManyStudentsException {
         List<Student> incomingStudents = new ArrayList<>(Arrays.asList(students));
         if (studentList.size() + incomingStudents.size() > maxSize) throw new TooManyStudentsException();
         studentList.addAll(Arrays.asList(students));
     }
 
+    /**
+     * Get students on cloud tile as a list
+     *
+     * @return a {@link List} of {@link Student} containing students on cloud tile
+     */
     public List<Student> getStudents() {
         return studentList;
     }
 
+    /**
+     * Remove students from cloud tile
+     *
+     * @return a {@link List} of {@link Student} containing all students removed from cloud tile
+     */
     public List<Student> getAndRemoveStudents() {
         List<Student> students = new ArrayList<>(studentList);
         studentList.clear();
         return students;
     }
 
+    /**
+     * Generate a string containing cloud tile info
+     *
+     * @return a string containing students inside cloud tile
+     */
     @Override
     public String toString() {
         String students = studentList.stream()
