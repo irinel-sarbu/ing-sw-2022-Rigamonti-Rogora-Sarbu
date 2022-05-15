90b03e5943631d847c9fe03dad3b40c4892e661b
Irinel Sarbu <razvanirinel.sarbu@gmail.com>
2022-05-15 09:47:02 +0200

    started SceneController

---
+++ src/main/java/view/gui/SceneController.java
@@ -0,0 +1,15 @@
+package view.gui;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SceneController {
+    private static Stage stage;
+    private static Scene scene;
+    private static Parent parent;
+
+    public static void switchScene(String sceneFile) {
+
+    }
+}
