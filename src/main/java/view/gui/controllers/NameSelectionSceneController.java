215ec16528e4125c1d665b4fde517b7e89896b11
Alberto Rigamonti <alberto1.rigamonti@mail.polimi.it>
2022-05-20 16:54:13 +0200

    Added Name length check and reactive response

--- src/main/java/view/gui/controllers/NameSelectionSceneController.java
+++ src/main/java/view/gui/controllers/NameSelectionSceneController.java
@@ -4,18 +4,32 @@ package view.gui.controllers;
 import eventSystem.EventManager;
 import eventSystem.events.local.EUpdateNickname;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import util.Logger;
 
+import java.awt.*;
 
 
 public class NameSelectionSceneController implements GenericSceneController {
     public TextField name;
+    public Pane balloonText;
+    public Label errorLabel;
 
     @FXML
     public void onNameConfirmed(MouseEvent mouseEvent) {
-        Logger.info("Name Selected: " + name.getText());
-        EventManager.notify(new EUpdateNickname(name.getText()));
+        if (name.getText().length() <= 0) {
+            errorLabel.setText("Name too Short");
+            balloonText.setVisible(true);
+        } else if (name.getText().length() > 10) {
+            errorLabel.setText("Name too Long");
+            balloonText.setVisible(true);
+        } else {
+            balloonText.setVisible(false);
+            Logger.info("Name Selected: " + name.getText());
+            EventManager.notify(new EUpdateNickname(name.getText()));
+        }
     }
 }
