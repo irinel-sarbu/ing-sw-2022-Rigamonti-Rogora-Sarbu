bcf2ba669f2c248cacdf48d49117c5b15a2f4210
Matteo Rogora <matteo.rogora@live.it>
2022-05-27 17:29:14 +0200

    Complete IslandViewController

--- src/main/java/view/gui/controllers/IslandViewSceneController.java
+++ src/main/java/view/gui/controllers/IslandViewSceneController.java
@@ -35,19 +35,32 @@ public class IslandViewSceneController implements GenericSceneController {
 
     private int numOfClouds, numOfStudents;
 
-    @FXML
-    private void initialize(LightModel model) {
+    private void init(LightModel model) {
         this.model = model;
         numOfClouds = model.getCloudTiles().size();
-        numOfStudents = numOfClouds == 3 ? 4 : 3;
+        numOfStudents = numOfClouds + 1;
         bridges = new ArrayList<>(bridges_parent.getChildren());
         islands = realm_parent.getChildren().subList(0, 12);
         clouds = new ArrayList<>(realm_parent.getChildren().subList(12, 15));
     }
 
+    private int groupByIslandID(int ID) {
+        for(IslandGroup ig : model.getIslandGroups()){
+            if(ig.getIslands().stream().map(IslandTile::getIslandID).toList().contains(ID))
+                return ig.getIslandGroupID();
+        }
+        return -1;
+    }
+
+    private boolean inSameGroup(int a, int b) {
+        return groupByIslandID(a) == groupByIslandID(b);
+    }
+
     // Display functions
-    private void showBridge(int a, int b) {
-        bridges.get(a).setVisible(true);
+    private void updateBridge() {
+        for(int i=0; i<12; i++) {
+            if(inSameGroup(i, (i+1)%12)) bridges.get(i).setVisible(true);
+        }
     }
 
     private void updateStudents(int island, Color color, int number) {
@@ -108,12 +121,24 @@ public class IslandViewSceneController implements GenericSceneController {
         }
     }
 
+    private void updateClouds(int cloudID, List<Color> studentList) {
+        AnchorPane cloud = (AnchorPane) clouds.get(cloudID);
+        List<Node> students = new ArrayList<>(cloud.getChildren().subList(1,5));
+        for(int i=0; i<studentList.size(); i++) {
+            (students.get(i)).setVisible(true);
+            ((ImageView)students.get(i)).setImage(new Image(pathPrefix + "students/" + studentList.get(i) + ".png"));
+        }
+    }
+
     private void resetView() {
         resetClouds();
         resetNoEntry();
     }
 
     public void updateView(LightModel model) {
+
+        init(model);
+        resetView();
 
         List<IslandTile> allIslands = new ArrayList<>();
         for (IslandGroup ig : model.getIslandGroups()) {
