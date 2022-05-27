751776fc204778b919da4092bd8883b6adbfc1b4
Alberto Rigamonti <alberto1.rigamonti@mail.polimi.it>
2022-05-27 14:08:37 +0200

    Finished student movement to dining

--- src/main/java/view/gui/controllers/EntranceSceneController.java
+++ src/main/java/view/gui/controllers/EntranceSceneController.java
@@ -1,5 +1,7 @@
 package view.gui.controllers;
 
+import eventSystem.EventManager;
+import eventSystem.events.network.client.actionPhaseRelated.EStudentMovementToDining;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -15,53 +17,90 @@ public class EntranceSceneController implements GenericSceneController {
     private ImageView student_0, student_1, student_2, student_3, student_4, student_5, student_6, student_7, student_8;
 
     private List<ImageView> students = new ArrayList<>();
+    private List<Student> entrance = new ArrayList<>();
     private LightModel model;
     //TODO: add dynamic scene switch, if toIsland is true go to islandScene selector, otherwise Dining Color Scene Selector
     private boolean toIsland;
 
     @FXML
     public void onStudent0(MouseEvent mouseEvent) {
+        if (toIsland) {
 
+        } else {
+            EventManager.notify(new EStudentMovementToDining(entrance.get(0).getID()));
+        }
     }
 
     @FXML
     public void onStudent1(MouseEvent mouseEvent) {
+        if (toIsland) {
 
+        } else {
+            EventManager.notify(new EStudentMovementToDining(entrance.get(1).getID()));
+        }
     }
 
     @FXML
     public void onStudent2(MouseEvent mouseEvent) {
+        if (toIsland) {
 
+        } else {
+            EventManager.notify(new EStudentMovementToDining(entrance.get(2).getID()));
+        }
     }
 
     @FXML
     public void onStudent3(MouseEvent mouseEvent) {
+        if (toIsland) {
 
+        } else {
+            EventManager.notify(new EStudentMovementToDining(entrance.get(3).getID()));
+        }
     }
 
     @FXML
     public void onStudent4(MouseEvent mouseEvent) {
+        if (toIsland) {
 
+        } else {
+            EventManager.notify(new EStudentMovementToDining(entrance.get(4).getID()));
+        }
     }
 
     @FXML
     public void onStudent5(MouseEvent mouseEvent) {
+        if (toIsland) {
 
+        } else {
+            EventManager.notify(new EStudentMovementToDining(entrance.get(5).getID()));
+        }
     }
 
     @FXML
     public void onStudent6(MouseEvent mouseEvent) {
+        if (toIsland) {
 
+        } else {
+            EventManager.notify(new EStudentMovementToDining(entrance.get(6).getID()));
+        }
     }
 
     @FXML
     public void onStudent7(MouseEvent mouseEvent) {
+        if (toIsland) {
 
+        } else {
+            EventManager.notify(new EStudentMovementToDining(entrance.get(7).getID()));
+        }
     }
 
     @FXML
     public void onStudent8(MouseEvent mouseEvent) {
+        if (toIsland) {
 
+        } else {
+            EventManager.notify(new EStudentMovementToDining(entrance.get(8).getID()));
+        }
     }
 
     public void setUp(LightModel model, String playerName, boolean toIsland) {
@@ -77,10 +116,11 @@ public class EntranceSceneController implements GenericSceneController {
         students.add(student_7);
         students.add(student_8);
 
-        List<Student> entrance = model.getSchoolBoardMap().get(playerName).getEntranceStudents();
+        entrance = model.getSchoolBoardMap().get(playerName).getEntranceStudents();
 
         for (int i = 0; i < entrance.size(); i++) {
             students.get(i).setImage(new Image("/Graphical_Assets/students/" + entrance.get(i).getColor().toString() + "Student.png"));
+            students.get(i).setVisible(true);
         }
     }
 }
